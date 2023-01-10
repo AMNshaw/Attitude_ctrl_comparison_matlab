746eb217f459ff47633b2798960b3f33b2568738
AMNshaw <rosel94638@gmail.com>
2023-01-10 19:08:27 +0800

    ubuntu

--- attitude_geometry.m
+++ attitude_geometry.m
@@ -13,17 +13,21 @@ omega_d = zeros(3, 1);
 e_Omega = zeros(3, 1);
 
 J = diag([0.0820 0.0845 0.1377]);
-
+alpha_record = zeros(iteration, 1);
 
 R_init = angle2dcm(200*pi/180, 20*pi/180, 0); 
+R = R_init;
 R_d = eye(3, 3);
+e_z_d = R_d(:, 3);
 
 k_R = P.kR;
 k_Omega = P.kOmega;
 
 
 for i = 1:iteration
-    
+    e_z  = R(:, 3);
+    alpha = acos(dot(e_z, e_z_d));
+    alpha_record(i, :)= alpha;
     e_R = 1/2 * vee(R_d'*R - R'*R_d);
     e_Omega = omega - R'*R_d*omega_d;
     M = -k_R*e_R - k_Omega*e_Omega + cross(omega, J*omega);
@@ -37,6 +41,7 @@ for i = 1:iteration
     R = real(R);
 end
 
+plot(1:iteration, alpha_record(1:iteration, 1));
 
 
 
